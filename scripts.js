eec414fbbb4884d2cab20e14c851d44132b0fd36
ana.gaspar <ana.gaspar@moovefy.com.br>
2023-12-29 11:20:50 -0300

    Melhorias no JS

--- scripts.js
+++ scripts.js
@@ -10,3 +10,21 @@ sr.reveal('.project', {duration : 3000, origin:'bottom'})
 sr.reveal('.github-link', {duration : 3000, origin:'bottom'})
 sr.reveal('.conteiner-skills', {duration : 3000, origin:'left'})
 sr.reveal('.conteiner-descricao', {duration : 3000, origin:'right'})
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Espera o DOM ser totalmente carregado
+
+  const verMaisButton = document.querySelector('.github-link button');
+  const projetosEscondidos = document.querySelectorAll('.pro.escondido');
+
+  verMaisButton.addEventListener('click', function () {
+    // Adiciona ou remove a classe 'escondido' dos projetos
+    projetosEscondidos.forEach((projeto) => {
+      projeto.classList.toggle('escondido');
+    });
+
+    // Muda o texto do botão para refletir se os projetos estão visíveis ou não
+    const buttonText = verMaisButton.textContent === 'Ver mais' ? 'Ver menos' : 'Ver mais';
+    verMaisButton.textContent = buttonText;
+  });
+});

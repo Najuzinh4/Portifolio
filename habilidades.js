3711c2a17ac67071b8fe704812279adb2fd60ce5
ana.gaspar <ana.gaspar@moovefy.com.br>
2023-12-29 13:39:08 -0300

    Modificações nas habilidades

--- habilidades.js
+++ habilidades.js
@@ -1,14 +1,14 @@
 const skills = document.querySelectorAll('.skill-box');
 const descricao = document.querySelector('.texto-descricao');
 const sobreSkill = [
-                    '<p>HTML</p> <br> <p>É uma linguagem de marcação utilizada na construção de páginas na Web.</p> <br>',
-                    '<p>CSS</p> <br> <p>É um mecanismo para adicionar estilo a um documento web.</p> <br>',
-                    '<p>JavaScript</p> <br> <p>É uma linguagem de programação. Juntamente com HTML e CSS, é uma das três principais tecnologias da web.</p> <br>',
-                    '<p>C Sharp</p> <br> <p>C# é uma linguagem de programação da Microsoft amplamente usada para desenvolver aplicativos desktop, web e jogos.</p> <br>',
-                    '<p>Entity Framework Core</p> <br> <p></p> <br>',
-                    '<p>SQL</p> <br> <p>É uma linguagem de folhas de estilo. Basicamente o Sass potencializa o CSS.</p> <br>',
-                    '<p>Git</p> <br> <p>É um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software.</p> <br>',
-                    '<p>Github</p> <br> <p>É uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. </p> <br>'
+                    '<p>HTML</p> <br> <p>Comecei a estudar HTML em 2018, durante meu tempo na Etec, e aprofundei meu conhecimento ao usá-lo no meu trabalho de conclusão de curso (TCC).</p> <br>',
+                    '<p>CSS</p> <br> <p>Assim como o HTML, iniciei meus estudos em CSS em 2018 na Etec. Durante o TCC em 2020, aprimorei minhas habilidades para criar designs atraentes para a web.</p> <br>',
+                    '<p>JavaScript</p> <br> <p>Minha jornada com JavaScript começou em 2020, quando precisei aprender rapidamente para concluir meu TCC durante a pandemia. Desde então, utilizei essa linguagem poderosa em diversos projetos,inclusive nesse.</p> <br>',
+                    '<p>C Sharp</p> <br> <p>Desde 2019, tenho usado C# para desenvolver aplicativos desktop  web durante meu técnico. Meu conhecimento foi consolidado durante um estágio na área específico em .NET e EntityFrameWorkCore</p> <br>',
+                    '<p>PHP</p> <br> <p>Iniciei meus estudos em PHP em 2019 na Etec. Durante a pandemia em 2020, aprofundei meu conhecimento por conta própria e utilizei PHP em projetos significativos, incluindo meu TCC.</p> <br>',
+                    '<p>Java</p> <br> <p>Desde 2020, tenho programado em Java, incluindo um estágio na área. Em 2022, a disciplina de Sistemas de Informação na Unesp solidificou meu conhecimento em Java.</p> <br>',
+                    '<p>SQL</p> <br> <p>Desenvolvi habilidades avançadas em SQL, tanto durante meu curso técnico quanto em experiências profissionais. Atualmente, uso SQL diariamente para realizar operações complexas e simples também.</p> <br>',
+                    '<p>Github</p> <br> <p>ngressei no GitHub em 2023 e desde então finalizei dois cursos. Este ano, iniciei minha jornada prática no Git no ambiente de trabalho, complementado pela disciplina de Estrutura de Dados 2 na faculdade.</p> <br>'
 ]
 const dataInicio = [
                     (new Date(2021, 10, 1)).getTime(), // 0
